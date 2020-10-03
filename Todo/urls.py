3773cc38065bbcc12ccdead4c6d765fea8879b84
Girum-Haile <35455380+Girum-Haile@users.noreply.github.com>
2020-10-03 23:36:06 +0300

    update feature added

--- Todo/urls.py
+++ Todo/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import home, add_todo, delete_todo, edit
+from .views import home, add_todo, edit_todo, delete_todo
 
 app_name = 'Todo'
 
 urlpatterns = [
     path('', home, name='home'),
     path('add_todo/', add_todo, name="add_todo"),
-    path('delete_todo/<int:todo_id>/', delete_todo, name="delete_todo"),
-    path('edit/<int:todo_id>/', edit, name="edit")
+    path('edit_todo/<int:todo_id>/', edit_todo, name="edit_todo"),
+    path('delete_todo/<int:todo_id>/', delete_todo),
+
 ]
--- Todo/views.py
+++ Todo/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
 from Todo.models import Todo
-from .forms import TodoForm
 from django.http import HttpResponseRedirect
 
 
@@ -13,6 +13,7 @@ def home(request):
     })
 
 
+@csrf_exempt
 def add_todo(request):
     current_date = timezone.now()
     content = request.POST["content"]
@@ -21,18 +22,24 @@ def add_todo(request):
     return HttpResponseRedirect("/")
 
 
+@csrf_exempt
+def edit_todo(request, todo_id):
+    todo_items = Todo.objects.all().order_by("-added_date")
+    target_item = todo_items.get(id=todo_id)
+
+    if request.method == 'POST':
+        content = request.POST["content"]
+        target_item.text = content
+        target_item.save()
+        return HttpResponseRedirect("/")
+    else:
+        return render(request, 'Todo/index.html', {
+        "todo_items": todo_items, "item": target_item, "action": "edit"
+    })
+
+
+@csrf_exempt
 def delete_todo(request, todo_id):
     Todo.objects.get(id=todo_id).delete()
     return HttpResponseRedirect("/")
 
-
-def edit(request, todo_id):
-    if request.method == 'POST':
-        item = Todo.objects.get(pk=todo_id)
-        form = TodoForm(request.POST or None, instance=item)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        item = Todo.objects.get(pk=todo_id)
-        return render(request, 'Todo/edit.html', {'item': item})
